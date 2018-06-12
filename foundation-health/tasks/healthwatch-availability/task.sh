161e521c2d4f54303efb63369938c6e68c403acb
Peter Blum <pblum@pivotal.io>
2018-06-12 15:58:52 -0500

    add output

--- foundation-health/tasks/healthwatch-availability/task.sh
+++ foundation-health/tasks/healthwatch-availability/task.sh
@@ -3,11 +3,11 @@
 set -eu
 
 status=$(curl -X GET  healthwatch-api.$SYS_ENDPOINT/info)
+echo $status
 
 if [ "$status" = "HAPI is happy" ]; then
  exit 0
 fi
-
 echo "Cannot reach Healthwatch API!!"
 
 exit 1
