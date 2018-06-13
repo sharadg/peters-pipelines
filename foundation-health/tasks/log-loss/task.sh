75a3a8ca712b98271596a8ccdeab90c876d63bab
Peter Blum <pblum@pivotal.io>
2018-06-13 12:48:13 -0500

    abstract IPs out of tasks

--- foundation-health/tasks/log-loss/task.sh
+++ foundation-health/tasks/log-loss/task.sh
@@ -2,7 +2,7 @@
 
 set -xu
 
-output=$(mysql -uadmin -p$HEALTHWATCH_MYSQL_PASSWORD -h10.0.5.121 platform_monitoring -B -e "select value from super_value_metric where name='Firehose.LossRate.1H' order by timestamp desc limit 1;" | tail -1)
+output=$(mysql -uadmin -p$HEALTHWATCH_MYSQL_PASSWORD -h$HEALTHWATCH_MYSQL_IP platform_monitoring -B -e "select value from super_value_metric where name='Firehose.LossRate.1H' order by timestamp desc limit 1;" | tail -1)
 threshold=0.01
 
 read status <<< $(echo $threshold $output | awk '{if ($1 < $2) print 0; else print 1}')
