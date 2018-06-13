5f2b1a783911e34abd79b12dceda022ccce902d1
Peter Blum <pblum@pivotal.io>
2018-06-13 08:58:29 -0500

    add nozzle

--- foundation-health/tasks/cli/task.sh
+++ foundation-health/tasks/cli/task.sh
@@ -3,7 +3,8 @@
 set -xu
 
 cf login -a https://api.$SYS_ENDPOINT  -u admin -p $ADMIN_PASSWORD -o system -s system --skip-ssl-validation
-
+cf add-plugin-repo CF-Community https://plugins.cloudfoundry.org
+cf install-plugin -r CF-Community "Firehose Plugin" -f
 output=$(cf nozzle --debug -f ValueMetric | grep -i -m 1 health.check.cliCommand.success)
 cleaned=$(echo $output | sed -n -e 's/^.*health.check.cliCommand.success" value//p')
 healthStatus=${cleaned:1:2}
