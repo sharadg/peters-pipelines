4ab6d532c0a2212d5fb042b0f992faa7d8cfad8b
Peter Blum <pblum@pivotal.io>
2018-06-14 11:25:51 -0500

    check status

--- foundation-health/tasks/log-loss/task.sh
+++ foundation-health/tasks/log-loss/task.sh
@@ -6,6 +6,12 @@ sysdomain=$(echo $CF_API_URL | sed 's~http[s]*://api.~~g')
 status=$(curl -k hw-data-access.$sysdomain/v1/logloss | jq ".status")
 threshold=0.01
 
+if [ -z "$status" ]
+ then
+    echo "Problem hitting hw-data-access app"
+    exit 1
+fi  
+
 read output <<< $(echo $threshold $status | awk '{if ($1 < $2) print 0; else print 1}')
 echo $output
 
