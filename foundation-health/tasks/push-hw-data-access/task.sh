2d9ffc74d1c8d9e27aec7126e6143e873534ff55
Peter Blum <pblum@pivotal.io>
2018-06-14 09:58:46 -0500

    add route

--- foundation-health/tasks/push-hw-data-access/task.sh
+++ foundation-health/tasks/push-hw-data-access/task.sh
@@ -6,6 +6,13 @@ cf login -a $CF_API_URL  -u admin -p $CF_PASSWORD --skip-ssl-validation -o syste
 cf create-space hw-data-access
 cf target -o system -s hw-data-access
 cd hw-data-access
+sysdomain=$(echo $CF_API_URL | sed 's~http[s]*://api.~~g')
+echo "cf sys domain: $sysdomain"
+echo "" >> manifest.yml
+echo "    routes:" >> manifest.yml
+echo "    - route: $sysdomain" >> manifest.yml
+echo "Manifest: "
+cat manifest.yml
 cf push -f manifest.yml --no-start
 cf set-env hw-data-access HEALTHWATCH_DB_IP $HEALTHWATCH_MYSQL_IP
 cf set-env hw-data-access HEALTHWATCH_DB_PASSWORD $HEALTHWATCH_MYSQL_ADMIN_PASSWORD
