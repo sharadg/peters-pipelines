2e93f4b85c5f34546eea0fbca3db818d05413778
Peter Blum <pblum@pivotal.io>
2018-06-14 20:35:18 -0500

    enable pipeline to be easier

--- foundation-health/tasks/push-hw-data-access/task.sh
+++ foundation-health/tasks/push-hw-data-access/task.sh
@@ -2,7 +2,11 @@
 
 set -eux
 
-cf login -a $CF_API_URL  -u admin -p $CF_PASSWORD --skip-ssl-validation -o system -s system
+healthwatch_db_password=$(om -k -t "$OPSMAN_URL" -u "$OPSMAN_USER" -p "$OPSMAN_PASSWORD" credentials -p p-healthwatch -c .mysql.mysql_admin_password -f password)
+healthwatch_product_guid=$(om -k -t "$OPSMAN_URL" -u "$OPSMAN_USER" -p "$OPSMAN_PASSWORD" curl -p /api/v0/deployed/products | jq '.[] | select(.installation_name | contains("p-healthwatch")) | .guid')
+healthwatch_db_ip=$(om -k -t "$OPSMAN_URL" -u "$OPSMAN_USER" -p "$OPSMAN_PASSWORD" curl -p /api/v0/deployed/products/$healthwatch_product_guid/status | jq -r '."status"[] | select(."job-name" | contains("mysql")) | .ips[0]')
+cf_admin_password=$(om -k -t "$OPSMAN_URL" -u "$OPSMAN_USER" -p "$OPSMAN_PASSWORD" credentials -p cf -c .uaa.admin_credentials -f password)
+cf login -a $CF_API_URL  -u admin -p $cf_admin_password --skip-ssl-validation -o system -s system
 
 set +e
 
@@ -20,7 +24,7 @@ echo "    - route: hw-data-access.$sysdomain" >> manifest.yml
 echo "Manifest: "
 cat manifest.yml
 cf push -f manifest.yml
-cf set-env hw-data-access HEALTHWATCH_DB_IP $HEALTHWATCH_MYSQL_IP
-cf set-env hw-data-access HEALTHWATCH_DB_PASSWORD $HEALTHWATCH_MYSQL_ADMIN_PASSWORD
+cf set-env hw-data-access HEALTHWATCH_DB_IP $healthwatch_db_ip
+cf set-env hw-data-access HEALTHWATCH_DB_PASSWORD $healthwatch_db_password
 cf restage hw-data-access
 
