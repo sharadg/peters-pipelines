625e60e7b6d199f5c2a3b198e2c7bc8eccd3ea99
Peter Blum <pblum@pivotal.io>
2018-06-14 20:37:05 -0500

    fix spelling

--- foundation-health/tasks/push-hw-data-access/task.sh
+++ foundation-health/tasks/push-hw-data-access/task.sh
@@ -2,10 +2,10 @@
 
 set -eux
 
-healthwatch_db_password=$(om -k -t "$OPSMAN_URL" -u "$OPSMAN_USER" -p "$OPSMAN_PASSWORD" credentials -p p-healthwatch -c .mysql.mysql_admin_password -f password)
-healthwatch_product_guid=$(om -k -t "$OPSMAN_URL" -u "$OPSMAN_USER" -p "$OPSMAN_PASSWORD" curl -p /api/v0/deployed/products | jq '.[] | select(.installation_name | contains("p-healthwatch")) | .guid')
-healthwatch_db_ip=$(om -k -t "$OPSMAN_URL" -u "$OPSMAN_USER" -p "$OPSMAN_PASSWORD" curl -p /api/v0/deployed/products/$healthwatch_product_guid/status | jq -r '."status"[] | select(."job-name" | contains("mysql")) | .ips[0]')
-cf_admin_password=$(om -k -t "$OPSMAN_URL" -u "$OPSMAN_USER" -p "$OPSMAN_PASSWORD" credentials -p cf -c .uaa.admin_credentials -f password)
+healthwatch_db_password=$(om -k -t "$OPSMAN_URL" -u "$OPSMAN_USERNAME" -p "$OPSMAN_PASSWORD" credentials -p p-healthwatch -c .mysql.mysql_admin_password -f password)
+healthwatch_product_guid=$(om -k -t "$OPSMAN_URL" -u "$OPSMAN_USERNAME" -p "$OPSMAN_PASSWORD" curl -p /api/v0/deployed/products | jq '.[] | select(.installation_name | contains("p-healthwatch")) | .guid')
+healthwatch_db_ip=$(om -k -t "$OPSMAN_URL" -u "$OPSMAN_USERNAME" -p "$OPSMAN_PASSWORD" curl -p /api/v0/deployed/products/$healthwatch_product_guid/status | jq -r '."status"[] | select(."job-name" | contains("mysql")) | .ips[0]')
+cf_admin_password=$(om -k -t "$OPSMAN_URL" -u "$OPSMAN_USERNAME" -p "$OPSMAN_PASSWORD" credentials -p cf -c .uaa.admin_credentials -f password)
 cf login -a $CF_API_URL  -u admin -p $cf_admin_password --skip-ssl-validation -o system -s system
 
 set +e
