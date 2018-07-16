ecf5bde349da124fb8047d527f005f84489c4259
Peter Blum <pblum@pivotal.io>
2018-07-15 20:57:46 -0500

    add check for loadbalancer ip

--- kubernetes/tasks/create-prometheus-loadbalancer/task.sh
+++ kubernetes/tasks/create-prometheus-loadbalancer/task.sh
@@ -7,3 +7,17 @@ cp pks-config/config ~/.kube/config
 kubectl create -f pks-prometheus/loadBalancer.yml -n $K8S_NAMESPACE
 
 kubectl get services -n $K8S_NAMESPACE
+
+while [ 1 ]
+do
+    loadBalancer_ip=$(kubectl get services -n monitoring -o json | jq -r ' .items[] | select(.metadata.name == "prometheus-service") | .status.loadBalancer.ingress[0].ip'
+    echo $loadBalancer_ip
+    if [ "$loadBalancer_ip" = "" ]
+    then
+        echo "Checking again..."
+    else
+        echo "Check out your Prometheus Dashboard! http://$loadbalancer_ip:8080"
+        exit 0 
+    fi
+    sleep 3
+done
