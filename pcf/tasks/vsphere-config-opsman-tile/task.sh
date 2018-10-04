1e7c35fabed25349283386568b2c4db071900024
Peter Blum <pblum@pivotal.io>
2018-10-03 23:22:53 -0500

    add config ops man tile

---
+++ pcf/tasks/vsphere-config-opsman-tile/task.sh
@@ -0,0 +1,155 @@
+#!/bin/bash
+
+set -eu
+
+iaas_configuration=$(
+  jq -n \
+  --arg vcenter_host "$VCENTER_HOST" \
+  --arg vcenter_username "$VCENTER_USR" \
+  --arg vcenter_password "$VCENTER_PWD" \
+  --arg datacenter "$VCENTER_DATA_CENTER" \
+  --arg disk_type "$VCENTER_DISK_TYPE" \
+  --arg ephemeral_datastores_string "$EPHEMERAL_STORAGE_NAMES" \
+  --arg persistent_datastores_string "$PERSISTENT_STORAGE_NAMES" \
+  --arg bosh_vm_folder "$BOSH_VM_FOLDER" \
+  --arg bosh_template_folder "$BOSH_TEMPLATE_FOLDER" \
+  --arg bosh_disk_path "$BOSH_DISK_PATH" \
+  --argjson ssl_verification_enabled false \
+  --argjson nsx_networking_enabled "$NSX_NETWORKING_ENABLED" \
+  --arg nsx_address "$NSX_ADDRESS" \
+  --arg nsx_username "$NSX_USERNAME" \
+  --arg nsx_password "$NSX_PASSWORD" \
+  --arg nsx_ca_certificate "$NSX_CA_CERTIFICATE" \
+  '
+  {
+    "vcenter_host": $vcenter_host,
+    "vcenter_username": $vcenter_username,
+    "vcenter_password": $vcenter_password,
+    "datacenter": $datacenter,
+    "disk_type": $disk_type,
+    "ephemeral_datastores_string": $ephemeral_datastores_string,
+    "persistent_datastores_string": $persistent_datastores_string,
+    "bosh_vm_folder": $bosh_vm_folder,
+    "bosh_template_folder": $bosh_template_folder,
+    "bosh_disk_path": $bosh_disk_path,
+    "ssl_verification_enabled": $ssl_verification_enabled,
+    "nsx_networking_enabled": $nsx_networking_enabled,
+  }
+
+  +
+
+  # NSX networking. If not enabled, the following section is not required
+  if $nsx_networking_enabled then
+    {
+      "nsx_address": $nsx_address,
+      "nsx_username": $nsx_username,
+      "nsx_password": $nsx_password,
+      "nsx_ca_certificate": $nsx_ca_certificate
+    }
+  else
+    .
+  end
+  '
+)
+
+az_configuration=$(cat <<-EOF
+ [
+    {
+      "name": "$AZ_1",
+      "cluster": "$AZ_1_CLUSTER_NAME",
+      "resource_pool": "$AZ_1_RP_NAME"
+    }
+ ]
+EOF
+)
+
+network_configuration=$(
+  jq -n \
+    --argjson icmp_checks_enabled $ICMP_CHECKS_ENABLED \
+    --arg network_name "$NETWORK_NAME" \
+    --arg vcenter_network "$VCENTER_NETWORK" \
+    --arg network_cidr "$NW_CIDR" \
+    --arg reserved_ip_ranges "$EXCLUDED_RANGE" \
+    --arg dns "$NW_DNS" \
+    --arg gateway "$NW_GATEWAY" \
+    --arg availability_zones "$NW_AZS" \
+    '
+    {
+      "icmp_checks_enabled": $icmp_checks_enabled,
+      "networks": [
+        {
+          "name": $network_name,
+          "service_network": false,
+          "subnets": [
+            {
+              "iaas_identifier": $vcenter_network,
+              "cidr": $network_cidr,
+              "reserved_ip_ranges": $reserved_ip_ranges,
+              "dns": $dns,
+              "gateway": $gateway,
+              "availability_zone_names": ($availability_zones | split(","))
+            }
+          ]
+        }
+      ]
+    }'
+)
+
+director_config=$(cat <<-EOF
+{
+  "ntp_servers_string": "$NTP_SERVERS",
+  "resurrector_enabled": $ENABLE_VM_RESURRECTOR,
+  "max_threads": $MAX_THREADS,
+  "database_type": "internal",
+  "blobstore_type": "local",
+  "director_hostname": "$OPS_DIR_HOSTNAME"
+}
+EOF
+)
+
+security_configuration=$(
+  jq -n \
+    --arg trusted_certificates "$TRUSTED_CERTIFICATES" \
+    '
+    {
+      "trusted_certificates": $trusted_certificates,
+      "vm_password_type": "generate"
+    }'
+)
+
+network_assignment=$(
+jq -n \
+  --arg infra_availability_zones "$INFRA_NW_AZS" \
+  --arg network "$INFRA_NETWORK_NAME" \
+  '
+  {
+  "singleton_availability_zone": {
+    "name": ($infra_availability_zones | split(",") | .[0])
+  },
+  "network": {
+    "name": $network
+  }
+  }'
+)
+
+echo "Configuring IaaS, AZ and Director..."
+om-linux \
+  --target https://$OPSMAN_DOMAIN_OR_IP_ADDRESS \
+  --skip-ssl-validation \
+  --username "$OPS_MGR_USR" \
+  --password "$OPS_MGR_PWD" \
+  configure-director \
+  --iaas-configuration "$iaas_configuration" \
+  --director-configuration "$director_config" \
+  --az-configuration "$az_configuration"
+
+echo "Configuring Network and Security..."
+om-linux \
+  --target https://$OPSMAN_DOMAIN_OR_IP_ADDRESS \
+  --skip-ssl-validation \
+  --username "$OPS_MGR_USR" \
+  --password "$OPS_MGR_PWD" \
+  configure-director \
+  --networks-configuration "$network_configuration" \
+  --network-assignment "$network_assignment" \
+  --security-configuration "$security_configuration"
