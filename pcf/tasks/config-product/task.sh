225bb284f3bcd06737f3e4f215a9066f1cfd7665
Peter Blum <pblum@pivotal.io>
2018-10-04 20:53:03 -0500

    add configs for mysql and rabbit

---
+++ pcf/tasks/config-product/task.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -eu
+
+function main() {
+
+  om-linux \
+    --target "https://${OPSMAN_DOMAIN_OR_IP_ADDRESS}" \
+    --username "${OPS_MGR_USR}" \
+    --password "${OPS_MGR_PWD}" \
+    --skip-ssl-validation \
+    configure-product \
+    --product-name "${PRODUCT_NAME}" \
+    --config "${PRODUCT_CONFIG_YAML}"
+}
+
+main
