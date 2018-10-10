a89d887dbe79e2f7a20694411320276259c4d6e7
Peter Blum <pblum@pivotal.io>
2018-10-10 18:46:53 -0500

    need to quote unzip

--- pas/tasks/inject-pasw-tile/task.sh
+++ pas/tasks/inject-pasw-tile/task.sh
@@ -3,7 +3,7 @@
 cp raw-pasw-tile/metadata.json pivnet-product/metadata.json
 cp raw-pasw-tile/metadata.yaml pivnet-product/metadata.yaml
 
-unzip raw-pasw-tile/*.zip
+unzip "raw-pasw-tile/*.zip"
 
 chmod +x winfs-injector-linux
 mv winfs-injector-linux /usr/local/bin/winfs-injector
