baff2477d0b2ec494b5af75deaf15508b00e3734
Peter Blum <pblum@pivotal.io>
2018-10-12 14:12:59 -0500

    fix ci skip

--- pks/tasks/determine-clusters-needs/task.sh
+++ pks/tasks/determine-clusters-needs/task.sh
@@ -20,7 +20,7 @@ rm -f pksRun.sh
 
 set -u
 time=$(date)
-echo "echo \"PKS cluster needs determined at: $time \"" > pksRun.sh
+printf "printf \"PKS cluster needs determined at: $time \"" > pksRun.sh
 
 pks-diff -current current.json -desired ${PKS_CLUSTER_JSON_FILE}
 
@@ -31,4 +31,4 @@ cat pksRun.sh
 git config user.email "pks-bot@pivotal.io"
 git config user.name "pks-bot"
 git add .
-git commit -m "Add Run Script For Getting Back To Desired State [ci skip]"
+git commit -m "[skip] [ci-skip] [ci skip] Add Run Script For Getting Back To Desired State"
