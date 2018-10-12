c5e0a5431fb14bb652613763137dea7a97f9e515
Peter Blum <pblum@pivotal.io>
2018-10-11 23:00:58 -0500

    make sure to use env variable

--- pks/tasks/determine-clusters-needs/task.sh
+++ pks/tasks/determine-clusters-needs/task.sh
@@ -3,9 +3,9 @@
 set -xu
 
 cp pks-config/creds.yml ~/.pks/creds.yml 
-cat pks-clusters/PKS_CLUSTER_JSON_FILE
+cat pks-clusters/${PKS_CLUSTER_JSON_FILE}
 
 pks clusters --json > pks-clusters/current.json
-pks-diff -current pks-clusters/current.json -desired pks-clusters/PKS_CLUSTER_JSON_FILE
+pks-diff -current pks-clusters/current.json -desired pks-clusters/${PKS_CLUSTER_JSON_FILE}
 
 cat pks-clusters/pksRun.sh
