7201453199e9d151e1460d86d4327e5d2fc0a861
Peter Blum <pblum@pivotal.io>
2018-10-11 23:06:37 -0500

    no need to show all commands

--- pks/tasks/determine-clusters-needs/task.sh
+++ pks/tasks/determine-clusters-needs/task.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -xu
+set -u
 
 cp pks-config/creds.yml ~/.pks/creds.yml
 cd pks-clusters
@@ -12,7 +12,9 @@ cat current.json
 echo "DESIRED STATE: "
 cat ${PKS_CLUSTER_JSON_FILE}
 
+set -x
 pks-diff -current current.json -desired ${PKS_CLUSTER_JSON_FILE}
 
+set +x
 echo "STEPS TO GET TO DESIRED STATE: "
 cat pksRun.sh
