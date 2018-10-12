b45873a6ba0736674134039ed6901e5f0406133c
Peter Blum <pblum@pivotal.io>
2018-10-11 23:12:08 -0500

    last of new lines to add

--- pks/tasks/determine-clusters-needs/task.sh
+++ pks/tasks/determine-clusters-needs/task.sh
@@ -7,14 +7,14 @@ cd pks-clusters
 
 pks clusters --json > current.json
 
-printf "\n\nCURRENT STATE: "
+printf "\n\nCURRENT STATE: \n"
 cat current.json
-printf "\n\nDESIRED STATE: "
+printf "\n\nDESIRED STATE: \n"
 cat ${PKS_CLUSTER_JSON_FILE}
 
 set -x
 pks-diff -current current.json -desired ${PKS_CLUSTER_JSON_FILE}
 
 set +x
-printf "\n\nSTEPS TO GET TO DESIRED STATE: "
+printf "\n\nSTEPS TO GET TO DESIRED STATE: \n"
 cat pksRun.sh
