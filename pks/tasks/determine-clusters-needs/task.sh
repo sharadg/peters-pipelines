03d01b055914bde1af685835cdbaa4264a3fac89
Peter Blum <pblum@pivotal.io>
2018-10-11 23:10:34 -0500

    printf is better

--- pks/tasks/determine-clusters-needs/task.sh
+++ pks/tasks/determine-clusters-needs/task.sh
@@ -7,14 +7,14 @@ cd pks-clusters
 
 pks clusters --json > current.json
 
-echo "\n\nCURRENT STATE: "
+printf "\n\nCURRENT STATE: "
 cat current.json
-echo "\n\nDESIRED STATE: "
+printf "\n\nDESIRED STATE: "
 cat ${PKS_CLUSTER_JSON_FILE}
 
 set -x
 pks-diff -current current.json -desired ${PKS_CLUSTER_JSON_FILE}
 
 set +x
-echo "\n\nSTEPS TO GET TO DESIRED STATE: "
+printf "\n\nSTEPS TO GET TO DESIRED STATE: "
 cat pksRun.sh
