278fe3d2b59bf28780273de776aa64f083ec306d
sharadg <91043+sharadg@users.noreply.github.com>
2018-11-17 14:46:24 -0600

    added a variable to convert k8s master hostname to ip

--- kubernetes/tasks/authenticate-kubernetes/task.sh
+++ kubernetes/tasks/authenticate-kubernetes/task.sh
@@ -5,13 +5,16 @@ set -xu
 
 pks login -a "$PKS_API_URL" -u "$PKS_API_USERNAME" -p "$PKS_API_PASSWORD" -k
 
-k8s_master_ip=$(pks cluster "$PKS_CLUSTER_NAME" --json | jq -r ".kubernetes_master_ips | first")
-
 export PKS_USER_PASSWORD="$PKS_API_PASSWORD"
 
 pks get-credentials "$PKS_CLUSTER_NAME"
 
-sed -i "s/server: .*$/server: https:\/\/"$k8s_master_ip":8443/" ~/.kube/config
+if [ "$PKS_CONVERT_HOST_TO_IP" = true ]
+then
+    echo "Converting K8s master hostname to ip address..."
+    k8s_master_ip=$(pks cluster "$PKS_CLUSTER_NAME" --json | jq -r ".kubernetes_master_ips | first")
+    sed -i "s/server: .*$/server: https:\/\/"$k8s_master_ip":8443/" ~/.kube/config
+fi
 
 kubectl cluster-info
 
